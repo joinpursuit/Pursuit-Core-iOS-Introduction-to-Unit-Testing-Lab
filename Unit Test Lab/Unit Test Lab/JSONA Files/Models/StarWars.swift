3e3fe5463b661f75a0b059ff7212fd29ae59ac37
Michelle <Michelle.cueva@gmail.com>
2019-08-29 23:48:35 -0400

    Adds tests for Star Wars Model

--- Unit Test Lab/Unit Test Lab/JSONA Files/Models/StarWars.swift
+++ Unit Test Lab/Unit Test Lab/JSONA Files/Models/StarWars.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+struct StarWarsWrapper: Codable {
+    let results: [StarWars]
+    
+    static func getStarWars(from data: Data) -> StarWarsWrapper? {
+        do {
+            let newStarWars = try JSONDecoder().decode(StarWarsWrapper.self, from: data)
+            return newStarWars
+            
+        } catch {
+            return nil
+        }
+    }
+    
+}
+
+struct StarWars: Codable {
+    let title: String
+    let opening_crawl: String
+}
+
--- Unit Test Lab/Unit Test LabTests/Unit_Test_LabTests.swift
+++ Unit Test Lab/Unit Test LabTests/Unit_Test_LabTests.swift
@@ -40,7 +40,7 @@ class Unit_Test_LabTests: XCTestCase {
         
     }
     
-    func testResultsCount() {
+    func testTriviaResultsCount() {
         let data = getTriviaDataFromJSON()
         guard let trivia = TriviaWrapper.getTrivia(from: data) else{return}
         XCTAssert(trivia.results.count == 10, "Count is not ten")
@@ -79,13 +79,38 @@ class Unit_Test_LabTests: XCTestCase {
         
     }
     
-    func testTriviaCount() {
+    func testJokesCount() {
         let data = getJokesDataFromJSON()
         guard let jokes = Jokes.getJokes(from: data) else{return}
         XCTAssert(jokes.count == 10, "Count is not ten")
     }
 
+    func testStarWars() {
+        let data = getStarWarsDataFromJSON()
+        guard let starWars = StarWarsWrapper.getStarWars(from: data) else {return}
+        XCTAssert(starWars != nil, "Does not exist")
+    }
     
+    
+    private func getStarWarsDataFromJSON() -> Data {
+        guard let pathToData = Bundle.main.path(forResource: "Star Wars API", ofType: "json") else {fatalError("did not find path")}
+        
+        let url = URL(fileURLWithPath: pathToData)
+        
+        do {
+            let data = try Data(contentsOf: url)
+            return data
+        } catch let jsonError {
+            fatalError("\(jsonError)")
+        }
+        
+    }
+    
+    func testSWResultsCount() {
+        let data = getStarWarsDataFromJSON()
+        guard let starWars = StarWarsWrapper.getStarWars(from: data) else {return}
+        XCTAssert(starWars.results.count == 7, "Count is not seven")
+    }
     
     
 
