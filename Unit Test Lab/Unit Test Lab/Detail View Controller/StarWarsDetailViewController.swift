5d4470013185e2196683b751902a5f237df42686
Michelle <Michelle.cueva@gmail.com>
2019-09-01 15:03:16 -0400

    Adds three cocoa files to use as  Detail View Controllers

---
+++ Unit Test Lab/Unit Test Lab/Detail View Controller/StarWarsDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StarWarsDetailViewController.swift
+//  Unit Test Lab
+//
+//  Created by Michelle Cueva on 9/1/19.
+//  Copyright © 2019 Michelle Cueva. All rights reserved.
+//
+
+import UIKit
+
+class StarWarsDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
